466f79daf50a3c90bb629ed91eec5ae5f467bd35
Andrew Amesbury <andrew@amesbury.it>
2018-05-16 10:18:05 +1200

    update command line arguments, add ignore for setenv shell script, add Dockerfile

--- crashalert.js
+++ crashalert.js
@@ -1,16 +1,31 @@
 // Crash alerter for NZTA Traffic
 
-var   Twit            = require('twit')
-const commandLineArgs = require('command-line-args')
-const commandLineUsage= require('command-line-usage')
-var   cmdline         = require('./cmdline')
-
+var   Twit              = require('twit');
+const commandLineArgs   = require('command-line-args');
+const commandLineUsage  = require('command-line-usage');
+var   cmdline           = require('./cmdline');
+var   _                 = require('lodash');
 
 const options = commandLineArgs(cmdline.optionDefinitions())
+const usage = commandLineUsage(cmdline.usageDefinitions())
 
 if (options.help) {
-  const usage = commandLineUsage(cmdline.usageDefinitions())
-  console.log(usage)
+  console.log(usage);
 } else {
-  console.log(options)
+  if (_.isEmpty(options)) {
+    console.log(usage);
+    throw new Error("No parameters given.");
+  }
+  //console.log(options);
+  //startStream();
 }
+
+function followTweets(options){
+  var T = new Twitt({
+    consumer_key:         options.apikey,
+    consumer_secret:      options.apisecret,
+    access_token:         options.token,
+    access_token_secret:  options.tokensecret
+  })
+  var stream = T.stream('statuses/firehose')
+};
