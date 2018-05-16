633da920a5f001481f462ff05271c4daa82c33d5
Andrew Amesbury <andrew@amesbury.it>
2018-05-16 13:37:08 +1200

    simplify to just env vars

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ So I know whether to leave work or to stay in town this afternoon.
 
 * Twitter developer account (api key)
 * Amazon AWS account (for SES to send e-mail)
-* node
+* node (developed on 6)
 
 ## Setting up
 Create a Twitter App account at [https://developer.twitter.com/en.html](https://developer.twitter.com/en.html)
@@ -21,13 +21,16 @@ Twitter API requires 4 pieces of data:
 * An access token
 * An access key
 
-# Running the app
-The parameters can either be fed in as command line arguments or set as environment variables.
+## Running the app
+The following parameters must be set as environment variables.
 
-If you are setting environment variables, ensure you `source` your environment variables correctly so they are visible in all sessions.
+```
+TWITTER_CONSUMER_KEY=""
+TWITTER_CONSUMER_SECRET=""
+TWITTER_ACCESS_TOKEN=""
+TWITTER_ACCESS_KEY=""
+```
 
-If you want to pass parameters then do as follows:
-```
-$ node crashalert.js --apikey <TWITTER_CONSUMER_KEY> --apisecret <TWITTER_CONSUMER_SECRET> --token
-  <TWITTER_ACCESS_TOKEN> --tokensecret <TWITTER_ACCESS_KEY>
-```
+_Don't forget to npm install_
+
+`$ node crashalert.js`
--- crashalert.js
+++ crashalert.js
@@ -1,52 +1,32 @@
 // Crash alerter for NZTA Traffic
 
-var   Twitter           = require('twitter');
-const commandLineArgs   = require('command-line-args');
-const commandLineUsage  = require('command-line-usage');
-var   cmdline           = require('./cmdline');
-var   _                 = require('lodash');
+const Twitter = require('twitter');
+var    _      = require('lodash');
 
-const options = commandLineArgs(cmdline.optionDefinitions())
-const usage = commandLineUsage(cmdline.usageDefinitions())
+const client = new Twitter({
+  consumer_key:       process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:    process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key:   process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:process.env.TWITTER_ACCESS_KEY
+});
 
-var   twitterCredentials = {}
+const start = (callback) => {
+  client.stream('statuses/filter', {track: '@wgncrashalert'}, stream => {
+    stream.on('data', (tweet) => {
+      console.log(`@${tweet.user.screen_name} just said something!`);
+      console.log(tweet)
+    });
 
-if (options.help)
-{
-  console.log(usage);
-}
-else
-{
-  if (_.isEmpty(options)) {
-    twitterCredentials.apikey       = process.env.TWITTER_CONSUMER_KEY;
-    twitterCredentials.apisecret    = process.env.TWITTER_CONSUMER_SECRET;
-    twitterCredentials.token        = process.env.TWITTER_ACCESS_TOKEN;
-    twitterCredentials.tokensecret  = process.env.TWITTER_ACCESS_KEY;
-    console.log(twitterCredentials);
-    console.log(process.env);
-  }
-  else // use the command line args
-  {
-    twitterCredentials.apikey       = options.apikey;
-    twitterCredentials.apisecret    = options.apisecret;
-    twitterCredentials.token        = options.token;
-    twitterCredentials.tokensecret  = options.tokensecret;
-    // If that doesn't work then dump the help to console
-    if ( _.isNil(twitterCredentials.apikey))
-    {
-      console.log(usage);
-      throw new Error("No parameters given.");
-    }
-  }
-}
+    stream.on('error', (error) => {
+      console.log(`*** STREAM ERROR :( ***'`, error);
+      callback(error);
+    });
 
-function followTweets(options){
-  var T = new Twitt({
-    consumer_key:         options.apikey,
-    consumer_secret:      options.apisecret,
-    access_token:         options.token,
-    access_token_secret:  options.tokensecret
-  })
-  //var stream = T.stream('statuses/filter', {track: })
-  //stream.on()
+    callback(undefined);
+  });
 };
+
+start((err) => {
+  if (err) throw err.error;
+  console.log('Server running...');
+});
